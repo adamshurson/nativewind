547f2a5c72d4a30e856c9cb3344627a28816512f
adamshurson <shurson.adam@gmail.com>
2018-10-31 11:56:19 -0500

    cleaning up example app, started testing suite

---
+++ __tests__/rendering.js
@@ -0,0 +1,20 @@
+import NativeWind from '../index';
+import React from 'react';
+import { Text, View } from 'react-native';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import config from '../src/defaultConfig';
+
+configure({ adapter: new Adapter() });
+
+describe('Style Injection', () => {
+    it("Injected classes should appear in style prop", () => {
+        const wrapper = shallow(
+            <NativeWind>
+                <Text nativewind="text-blue">Hello</Text>
+            </NativeWind>
+        );
+        const colorClass = config.colors["blue"];
+        expect(wrapper.prop('style')[0].color).toEqual(colorClass);
+    });
+});
--- styleMap.js
+++
@@ -1,5 +0,0 @@
-export default {
-    'alignContent': {
-        
-    }
-}
